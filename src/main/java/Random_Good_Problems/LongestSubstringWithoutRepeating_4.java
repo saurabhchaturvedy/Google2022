48cfc869589ed5dfd07c23069388b46d7cadcd6a
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2022-07-22 11:29:26 -0700

    adding linked list notes and problems

---
+++ src/main/java/Random_Good_Problems/LongestSubstringWithoutRepeating_4.java
@@ -0,0 +1,36 @@
+package main.java.Random_Good_Problems;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubstringWithoutRepeating_4 {
+
+
+    private static int longestSubstring(String str) {
+        Map<Character, Integer> characterCountMap = new HashMap<>();
+
+        int max_length = 0;
+        int start = 0;
+
+        for (int end = 0; end < str.length(); end++) {
+
+            Character c = str.charAt(end);
+
+            if (characterCountMap.containsKey(c)) {
+                start = Math.max(start, characterCountMap.get(c) + 1);
+            }
+
+            characterCountMap.put(c, end);
+            max_length = Math.max(max_length, end - start + 1);
+        }
+
+        return max_length;
+    }
+
+    public static void main(String[] args) {
+
+        String str="pwwkew";
+
+        System.out.println("Longest substring without repeating characters is : "+longestSubstring(str));
+    }
+}
