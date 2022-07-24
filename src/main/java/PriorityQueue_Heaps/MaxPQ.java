629a5e909ec5f2bd8ea68aa479a60905010672d5
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2022-07-24 09:52:28 +0530

    heap max heap problems

--- src/main/java/PriorityQueue_Heaps/MaxPQ.java
+++ src/main/java/PriorityQueue_Heaps/MaxPQ.java
@@ -18,8 +18,49 @@ public class MaxPQ {
         return n;
     }
 
+    private void insert(int x) {
+        if (n == heap.length - 1) {
+            resize(2 * heap.length);
+        }
+
+        n++;
+        heap[n] = x;
+        swim(n);
+    }
+
+    private void swim(int k) {
+
+        while (k > 1 && heap[k / 2] < heap[k]) {
+            int temp = heap[k / 2];
+            heap[k / 2] = heap[k];
+            heap[k] = temp;
+            k = k / 2;
+        }
+    }
+
+
+    private void resize(int capacity) {
+        int[] temp = new int[capacity];
+        for (int i = 0; i < heap.length; i++) {
+            temp[i] = heap[i];
+        }
+
+        heap = temp;
+    }
+
+    private void printHeap() {
+        for (int i = 1; i <= n; i++) {
+            System.out.print(heap[i] + " ");
+        }
+    }
 
     public static void main(String[] args) {
+        MaxPQ maxPQ = new MaxPQ(4);
+        maxPQ.insert(5);
+        maxPQ.insert(8);
+        maxPQ.insert(3);
+        maxPQ.insert(7);
 
+        maxPQ.printHeap();
     }
 }
