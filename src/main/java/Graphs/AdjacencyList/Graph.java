d76768133eedf77d01d8054fef23e6531da82800
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2022-07-29 10:23:57 +0530

    changes done for tries problems

--- src/main/java/Graphs/AdjacencyList/Graph.java
+++ src/main/java/Graphs/AdjacencyList/Graph.java
@@ -133,6 +133,39 @@ public class Graph {
         }
     }
 
+
+    private int numberOfIslands(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        boolean[][] visited = new boolean[m][n];
+        int numberOfIslands = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!visited[i][j] && grid[i][j] == 1) {
+                    dfs(grid, i, j, visited);
+                    numberOfIslands++;
+                }
+            }
+        }
+
+        return numberOfIslands;
+    }
+
+    private void dfs(int[][] grid, int row, int col, boolean[][] visited) {
+
+        if (row < 0 || col < 0 || row >= grid.length || col >= grid[0].length || visited[row][col] || grid[row][col] == 0) {
+            return;
+        }
+
+        visited[row][col] = true;
+        dfs(grid, row, col - 1, visited);
+        dfs(grid, row - 1, col, visited);
+        dfs(grid, row, col + 1, visited);
+        dfs(grid, row + 1, col, visited);
+    }
+
     public static void main(String[] args) {
         Graph graph = new Graph(4);
         graph.addEdge(0, 1);
