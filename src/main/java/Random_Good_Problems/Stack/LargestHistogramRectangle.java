991c33526fe746c4b16583ce5be845765e4004b7
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2022-09-30 19:43:05 +0530

    largest rectangle in histogram

---
+++ src/main/java/Random_Good_Problems/Stack/LargestHistogramRectangle.java
@@ -0,0 +1,29 @@
+package main.java.Random_Good_Problems.Stack;
+
+import java.util.Stack;
+
+public class LargestHistogramRectangle {
+    public static int largestRectangleArea(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+
+        Stack<Integer> stack = new Stack<Integer>();
+        int max = 0;
+        for (int i = 0; i <= height.length; i++) {
+            int current = (i == height.length) ? -1 : height[i];
+            while (!stack.isEmpty() && current <= height[stack.peek()]) {
+                int h = height[stack.pop()];
+                int w = stack.isEmpty() ? i : i - stack.peek() - 1;
+                max = Math.max(max, h * w);
+            }
+            stack.push(i);
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(largestRectangleArea(new int[]{2, 1, 5, 6, 2, 3}));
+    }
+}
