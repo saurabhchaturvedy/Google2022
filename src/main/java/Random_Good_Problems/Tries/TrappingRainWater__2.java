d48aa4cad52278fd27c2de852b41c9b9dafedfd8
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2022-10-08 02:53:54 +0530

    Trapping Rain water different approach

---
+++ src/main/java/Random_Good_Problems/Tries/TrappingRainWater__2.java
@@ -0,0 +1,29 @@
+package main.java.Random_Good_Problems.Tries;
+
+class TrappingRainWater__2 {
+    public int trap(int[] height) {
+        int water = 0;
+        int left = 0;
+        int right = height.length - 1;
+        int leftTop = 0;
+        int rightTop = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                leftTop = Math.max(leftTop, height[left]);
+                water += leftTop - height[left];
+                left++;
+            } else {
+                rightTop = Math.max(rightTop, height[right]);
+                water += rightTop - height[right];
+                right--;
+            }
+        }
+        return water;
+    }
+
+    public static void main(String[] args) {
+        TrappingRainWater__2 trappingRainWater__2 = new TrappingRainWater__2();
+        System.out.println(trappingRainWater__2.trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
+    }
+}
