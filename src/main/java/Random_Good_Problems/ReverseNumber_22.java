2d65d27ce701041c3e7e9dc73e7c01d7eb908b5f
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2022-10-08 12:28:55 +0530

    Reverse Number

---
+++ src/main/java/Random_Good_Problems/ReverseNumber_22.java
@@ -0,0 +1,28 @@
+package main.java.Random_Good_Problems;
+
+public class ReverseNumber_22 {
+
+
+    private static int reverseNumber(int x) {
+
+        int remainder = 0;
+        int reversedNumber = 0;
+
+        while (x != 0) {
+
+            remainder = x % 10;
+            reversedNumber = reversedNumber * 10 + remainder;
+            x = x / 10;
+
+        }
+
+        return reversedNumber;
+
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println(reverseNumber(123));
+    }
+}
