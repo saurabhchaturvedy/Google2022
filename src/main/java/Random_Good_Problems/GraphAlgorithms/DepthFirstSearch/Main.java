7075970c57136a9d0b64012280fb8f58fdd8c2d3
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2022-10-08 16:22:16 +0530

    understanding designing Depth first search in recursive mode

--- src/main/java/Random_Good_Problems/GraphAlgorithms/DepthFirstSearch/Main.java
+++ src/main/java/Random_Good_Problems/GraphAlgorithms/DepthFirstSearch/Main.java
@@ -1,26 +1,48 @@
 package main.java.Random_Good_Problems.GraphAlgorithms.DepthFirstSearch;
 
+import main.java.Random_Good_Problems.GraphAlgorithms.BreadthFirstSearch.BreadthFirstSearch;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
+        DepthFirstSearch depthFirstSearch = new DepthFirstSearch();
+
         Vertex a = new Vertex("A");
         Vertex b = new Vertex("B");
         Vertex c = new Vertex("C");
         Vertex d = new Vertex("D");
         Vertex e = new Vertex("E");
+        Vertex f = new Vertex("F");
+        Vertex g = new Vertex("G");
+        Vertex h = new Vertex("H");
 
 
         a.addNeighbour(b);
-        a.addNeighbour(c);
-        c.addNeighbour(d);
+        a.addNeighbour(f);
+        a.addNeighbour(g);
+
+        b.addNeighbour(a);
+        b.addNeighbour(c);
+        b.addNeighbour(d);
+
+        c.addNeighbour(b);
+
         d.addNeighbour(e);
+        d.addNeighbour(b);
 
-        List<Vertex> vertexList = Arrays.asList(a, b, c, d, e);
+        e.addNeighbour(d);
 
-        DepthFirstSearch depthFirstSearch = new DepthFirstSearch();
+        f.addNeighbour(a);
+
+        g.addNeighbour(a);
+        g.addNeighbour(h);
+
+        h.addNeighbour(g);
+
+        List<Vertex> vertexList = Arrays.asList(a, b, c, d, e, f, g, h);
 
         depthFirstSearch.traverse(vertexList);
     }
