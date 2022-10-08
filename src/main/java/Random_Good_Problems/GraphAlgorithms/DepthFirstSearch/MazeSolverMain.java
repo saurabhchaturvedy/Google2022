03e6fa062a2e525f30df4107d40d4f5e42add078
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2022-10-08 17:31:53 +0530

    understanding the maze solver logic using depth first search

--- src/main/java/Random_Good_Problems/GraphAlgorithms/DepthFirstSearch/MazeSolverMain.java
+++ src/main/java/Random_Good_Problems/GraphAlgorithms/DepthFirstSearch/MazeSolverMain.java
@@ -1,2 +1,18 @@
-package main.java.Random_Good_Problems.GraphAlgorithms.DepthFirstSearch;public class MazeSolverMain {
+package main.java.Random_Good_Problems.GraphAlgorithms.DepthFirstSearch;
+
+public class MazeSolverMain {
+
+    public static void main(String[] args) {
+        int[][] maze = {
+
+                {1, 1, 1, 1, 1},
+                {2, 0, 0, 1, 1},
+                {1, 1, 0, 0, 0},
+                {1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 0}
+        };
+
+        MazeSolver mazeSolver = new MazeSolver(maze, 1, 0);
+        mazeSolver.findWay();
+    }
 }
