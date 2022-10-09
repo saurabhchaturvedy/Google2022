fc1b490b9a6b8c75bed2f846c7d766eab01a3793
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2022-10-09 15:40:38 +0530

    understanding kth smallest element in bst problem

---
+++ src/main/java/Random_Good_Problems/BST/KthSmallestElement.java
@@ -0,0 +1,37 @@
+package main.java.Random_Good_Problems.BST;
+
+import java.util.Stack;
+
+public class KthSmallestElement {
+
+
+    public static Integer KthSmallestElement(TreeNode root, int k) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode node = root;
+
+        while (!stack.isEmpty() || node != null) {
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+        }
+
+
+        TreeNode top = stack.pop();
+        k--;
+
+        if (k == 0) {
+            node = top;
+            return node.data;
+        } else {
+            node = top.right;
+        }
+
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/Random_Good_Problems/BST/TreeNode.java
@@ -0,0 +1,14 @@
+package main.java.Random_Good_Problems.BST;
+
+public class TreeNode {
+
+    int data;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
