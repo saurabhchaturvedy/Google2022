f3841024b593f8694bd6b9e864e81fe72abd27eb
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2022-10-09 13:45:35 +0530

    depth first search and topological ordering

--- src/main/java/Random_Good_Problems/GraphAlgorithms/TopologicalOrdering/TopologicalOrdering.java
+++ src/main/java/Random_Good_Problems/GraphAlgorithms/TopologicalOrdering/TopologicalOrdering.java
@@ -1,2 +1,31 @@
-package main.java.Random_Good_Problems.GraphAlgorithms.TopologicalOrdering;public class TopologicalOrdering {
+package main.java.Random_Good_Problems.GraphAlgorithms.TopologicalOrdering;
+
+import java.util.Stack;
+
+public class TopologicalOrdering {
+
+
+    Stack<Vertex> stack;
+
+    TopologicalOrdering() {
+        this.stack = new Stack<>();
+    }
+
+    public void dfs(Vertex vertex) {
+        vertex.setVisited(true);
+
+        for (Vertex v : vertex.getNeighbours()) {
+            if (!v.isVisited()) {
+                dfs(v);
+            }
+
+
+        }
+
+        stack.push(vertex);
+    }
+
+    public Stack<Vertex> getTopologicalOrder() {
+        return stack;
+    }
 }
