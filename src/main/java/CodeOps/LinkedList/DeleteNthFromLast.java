976a4ee6afbfece8fa2f0194a703369efeb85edf
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2022-10-15 17:09:29 +0530

    codeops linked-list questions

---
+++ src/main/java/CodeOps/LinkedList/DeleteNthFromLast.java
@@ -0,0 +1,32 @@
+package main.java.CodeOps.LinkedList;
+
+public class DeleteNthFromLast {
+
+
+    private ListNode deleteNthFromLast(ListNode head, int n) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+
+        if (fast == null) {
+            head = head.next;
+            return head;
+        }
+
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+        return head;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/CodeOps/LinkedList/ListNode.java
@@ -0,0 +1,11 @@
+package main.java.CodeOps.LinkedList;
+
+public class ListNode {
+
+    int data;
+    ListNode next;
+
+    ListNode(int data) {
+        this.data = data;
+    }
+}
