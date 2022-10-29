7da03117d381e1d8aa4b13fe70ce3c0e0c3f1a2e
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2022-10-29 23:56:49 +0530

    separate +ve -ve

--- src/main/java/CodingSimplified_Nov2022/Array/_20/_17_SeparatePositiveNegative.java
+++ src/main/java/CodingSimplified_Nov2022/Array/_20/_17_SeparatePositiveNegative.java
@@ -1,2 +1,38 @@
-package main.java.CodingSimplified_Nov2022.Array._20;public class _17_SeparatePositiveNegative {
+package main.java.CodingSimplified_Nov2022.Array._20;
+
+import java.util.Arrays;
+
+public class _17_SeparatePositiveNegative {
+
+
+    public static void separatePositiveNegative(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            while (arr[start] < 0 && start < end) {
+                start++;
+            }
+
+            while (arr[end] > 0 && start < end) {
+                end--;
+            }
+
+            if (start < end) {
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+
+                start++;
+                end--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {-5, 2, 5, 7, -8, -4, 16, -9, 24};
+        separatePositiveNegative(arr);
+        Arrays.stream(arr).boxed().map(x -> x + " ").forEach(System.out::print);
+    }
 }
