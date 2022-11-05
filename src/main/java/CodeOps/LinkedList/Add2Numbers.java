aa0c369a8d67b50979d54e2e04f72a409e8ba784
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2022-11-05 18:40:25 +0530

    add 2 numbers

---
+++ src/main/java/CodeOps/LinkedList/Add2Numbers.java
@@ -0,0 +1,80 @@
+package main.java.CodeOps.LinkedList;
+
+public class Add2Numbers {
+
+
+    public ListNode addNumbers(ListNode first, ListNode second) {
+        first = reverse(first);
+        second = reverse(second);
+
+        ListNode fakeHead = new ListNode(0);
+        ListNode temp = fakeHead;
+
+        int sum = 0;
+        int carry = 0;
+
+        while (first != null || second != null) {
+            sum = ((first != null ? first.data : 0) + (second != null ? second.data : 0) + carry) % 10;
+            carry = sum / 10;
+
+            temp.next = new ListNode(sum);
+            temp = temp.next;
+
+            if (first != null) first = first.next;
+            if (second != null) second = second.next;
+        }
+
+        if (carry > 0) {
+            temp = new ListNode(carry);
+        }
+
+        return reverse(fakeHead.next);
+    }
+
+    private ListNode reverse(ListNode head) {
+
+        ListNode current = head;
+        ListNode previous = null;
+        ListNode next = null;
+
+        while (current != null) {
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+
+        return previous;
+
+    }
+
+    public void print(ListNode head) {
+        ListNode current = head;
+
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        Add2Numbers add2Numbers = new Add2Numbers();
+
+        ListNode first = new ListNode(3);
+        ListNode second = new ListNode(9);
+        ListNode third = new ListNode(0);
+
+        first.next = second;
+        second.next = third;
+
+        ListNode fourth = new ListNode(4);
+        ListNode fifth = new ListNode(5);
+
+        fourth.next = fifth;
+
+        add2Numbers.print(add2Numbers.addNumbers(first, fourth));
+
+    }
+}
