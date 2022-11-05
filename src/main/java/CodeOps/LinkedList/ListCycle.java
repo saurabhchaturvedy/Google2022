ebc1001922370774d8b85ac1e10e6975efb74e01
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2022-11-05 19:54:59 +0530

    linked list has cycles

---
+++ src/main/java/CodeOps/LinkedList/ListCycle.java
@@ -0,0 +1,26 @@
+package main.java.CodeOps.LinkedList;
+
+public class ListCycle {
+
+
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
