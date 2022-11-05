70d1e638ab5cb6a99141e9cd374417597e6e9e42
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2022-11-05 19:48:55 +0530

    reverse node k groups

--- src/main/java/CodeOps/LinkedList/ReverseNodeKGroups.java
+++ src/main/java/CodeOps/LinkedList/ReverseNodeKGroups.java
@@ -1,2 +1,35 @@
-package main.java.CodeOps.LinkedList;public class ReverseNodeKGroups {
+package main.java.CodeOps.LinkedList;
+
+public class ReverseNodeKGroups {
+
+
+    public ListNode reverseK(ListNode head, int k, int size) {
+        if (size < k) {
+            return head;
+        }
+        ListNode current = head;
+        ListNode previous = null;
+        ListNode next = null;
+
+        int count = 0;
+
+        while (current != null && count < k) {
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+            count++;
+        }
+
+        if (next != null) {
+            head.next = reverseK(next, k, size - k);
+        }
+
+        return previous;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
 }
