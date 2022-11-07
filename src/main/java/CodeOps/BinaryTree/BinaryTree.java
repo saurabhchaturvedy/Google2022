4894bb9f3489a8935ee1695e5c7f2936d0a5b700
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2022-11-07 16:38:08 +0530

    binary tree

---
+++ src/main/java/CodeOps/BinaryTree/BinaryTree.java
@@ -0,0 +1,10 @@
+package main.java.CodeOps.BinaryTree;
+
+public class BinaryTree {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(10);
+        root.left = new TreeNode(20);
+        root.right = new TreeNode(30);
+    }
+}
