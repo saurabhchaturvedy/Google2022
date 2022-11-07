1c581ffaa0374abb820c9d20998924fec588368f
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2022-11-07 16:38:35 +0530

    tree node

--- src/main/java/CodeOps/BinaryTree/TreeNode.java
+++ src/main/java/CodeOps/BinaryTree/TreeNode.java
@@ -1,2 +1,12 @@
-package main.java.CodeOps.BinaryTree;public class TreeNode {
+package main.java.CodeOps.BinaryTree;
+
+public class TreeNode {
+
+    TreeNode left;
+    TreeNode right;
+    int data;
+
+    TreeNode(int data) {
+        this.data = data;
+    }
 }
