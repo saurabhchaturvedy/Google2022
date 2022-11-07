b00f6f4aba526ccea11fbfd57e737b07e4db3eea
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2022-11-07 20:01:31 +0530

    diameter of the binary tree optimized

---
+++ src/main/java/CodeOps/BinaryTree/_10_Diameter_O.java
@@ -0,0 +1,43 @@
+package main.java.CodeOps.BinaryTree;
+
+public class _10_Diameter_O {
+
+    static class Pair {
+        int height;
+        int diameter;
+
+    }
+
+    public Pair diameter(TreeNode root) {
+        if (root == null) {
+            Pair p = new Pair();
+            p.height = -1;
+            p.diameter = 0;
+        }
+
+        Pair leftPair = diameter(root.left);
+        Pair rightPair = diameter(root.right);
+
+        Pair newPair = new Pair();
+
+        newPair.height = 1 + Math.max(leftPair.height, rightPair.height);
+
+        int a = 2 + leftPair.height + rightPair.height;
+
+        int diameter = Math.max(a, Math.max(leftPair.height, rightPair.height));
+
+        newPair.diameter = diameter;
+
+        return newPair;
+    }
+
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        Pair p = diameter(root);
+        return p.diameter;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
