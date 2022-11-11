f6cfdc070d4000ccc72d42563cf6d4391239e33f
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2022-11-11 20:48:18 +0530

    find peak element

---
+++ src/main/java/CodeOps/BinaryTree/_21_RootDistanceK.java
@@ -0,0 +1,2 @@
+package main.java.CodeOps.BinaryTree;public class _21_RootDistanceK {
+}
--- src/main/java/Random_Good_Problems/RandomGoodProblems_2/_7_FindPeakElement.java
+++ src/main/java/Random_Good_Problems/RandomGoodProblems_2/_7_FindPeakElement.java
@@ -18,18 +18,22 @@ class _7_FindPeakElement {
         int end = len - 1;
         while (start + 1 < end) {
             int mid = start + (end - start) / 2;
+            System.out.println(" mid = "+mid);
+            System.out.println("nums[mid] = "+nums[mid]+" nums[mid+1]="+nums[mid+1]);
             if (nums[mid] < nums[mid + 1]) {
                 start = mid;
+                System.out.println(" start "+start);
             } else {
                 end = mid;
+                System.out.println(" end "+end);
             }
         }
 
-        return (nums[start] > nums[start + 1]) ? start : end;
+        return (nums[start] > nums[start + 1]) ? nums[start] : nums[end];
     }
 
-	public static void main(String[] args) {
-		int[]arr= {5, 10, 20, 15};
-		System.out.println(findPeakElement(arr));
-	}
+    public static void main(String[] args) {
+        int[] arr = {5, 10, 20, 15};
+        System.out.println(findPeakElement(arr));
+    }
 }
