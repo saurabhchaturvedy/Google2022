72bd4d7bb86a6180eb49b0c13aabd11bcf8eb3cc
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2022-11-27 00:53:26 +0530

    MS OA

--- src/main/java/Random_Good_Problems/RandomGoodProblems_2/MicrosoftOA/LargestWoodenSquare.java
+++ src/main/java/Random_Good_Problems/RandomGoodProblems_2/MicrosoftOA/LargestWoodenSquare.java
@@ -15,6 +15,25 @@ class LargestWoodenSquare {
         return 0;
     }
 
+    public static int solution(int A, int B) {
+        int maxSize = 0;
+
+        if (A >= (B/3))
+            maxSize = Math.max(maxSize, B/3);
+        else
+            maxSize = Math.max(maxSize, A/1);
+        if (A >= 2*(B/2))
+            maxSize = Math.max(maxSize, B/2);
+        else
+            maxSize = Math.max(maxSize, A/2);
+        if (B >= (A/3))
+            maxSize = Math.max(maxSize, A/3);
+        else
+            maxSize = Math.max(maxSize, B/1);
+
+        return maxSize;
+    }
+
     public static void main(String[] args) {
 
         int A = 10; int B = 21;
@@ -22,7 +41,7 @@ class LargestWoodenSquare {
 
 
 //        System.out.println(largestWoodenSquare(13,11));
-        System.out.println(largestWoodenSquare(2,1));
+        System.out.println(solution(10,21));
 //        System.out.println(largestWoodenSquare(1,8));
     }
 }
