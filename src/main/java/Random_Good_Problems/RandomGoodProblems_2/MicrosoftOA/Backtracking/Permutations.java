c2d6936f8d4410b3852d4dfaab231969d2e53c12
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2022-11-26 15:10:43 +0530

    MS OA

--- src/main/java/Random_Good_Problems/RandomGoodProblems_2/MicrosoftOA/Backtracking/Permutations.java
+++ src/main/java/Random_Good_Problems/RandomGoodProblems_2/MicrosoftOA/Backtracking/Permutations.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class Permutations {
 
     List<List<Integer>> res = new ArrayList<>();
+    List<String> stringPerms = new ArrayList<>();
 
     public List<List<Integer>> permute(int[] nums) {
         List<Integer> decisionTree = new LinkedList<>();
@@ -15,6 +16,16 @@ public class Permutations {
         }
         helper(decisionTree, new LinkedList<>());
         return res;
+    }
+
+    public List<String> permuteString(String str) {
+        List<Character> decisionTree = new LinkedList<>();
+        for (int ch : str.toCharArray()) {
+            Character c = (char) ch;
+            decisionTree.add(c);
+        }
+        helperString(decisionTree, new LinkedList<>());
+        return stringPerms;
     }
 
     private void helper(List<Integer> decisionTree, List<Integer> curComb) {
@@ -33,9 +44,32 @@ public class Permutations {
         }
     }
 
+
+    private void helperString(List<Character> decisionTree, List<Character> curComb) {
+        if (decisionTree.isEmpty()) {
+            char[] ch = new char[curComb.size()];
+            for (int i = 0; i < curComb.size(); i++) {
+                ch[i] = curComb.get(i);
+            }
+
+            stringPerms.add(new String(ch));
+            return;
+        }
+        for (int i = 0; i < decisionTree.size(); i++) {
+            Character val = decisionTree.remove(i);
+            curComb.add(val);
+            helperString(decisionTree, curComb);
+            // Reverse the changes to get ready for the next iteration
+            curComb.remove(curComb.size() - 1);
+            decisionTree.add(i, val);
+        }
+    }
+
     public static void main(String[] args) {
         int[] arr = {1, 2, 3};
         Permutations permutations1 = new Permutations();
         System.out.println(permutations1.permute(arr));
+
+        System.out.println(permutations1.permuteString("abc"));
     }
 }
